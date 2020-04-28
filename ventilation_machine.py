04ad6bf1b11242334eb4633664272c9a198016d1
GGodley <64490648+GGodley@users.noreply.github.com>
2020-04-28 20:20:04 +0300

    Add files via upload

--- ventilation_machine.py
+++ ventilation_machine.py
@@ -16,4 +16,26 @@
 from EEPROM import EEPROM  # R/W/CLR Taken from https://www.instructables.com/id/Raspberry-Pi-Python-EEPROM-Programmer/
 from LCI2C import liquidcrystal_i2c # taken form https://github.com/pl31/python-liquidcrystal_i2c/tree/master/liquidcrystal_i2c
 from  pigpio import servo #taken from https://www.raspberrypi.org/forums/viewtopic.php?t=82142
-from  SparkFun_MS5803_I2C
+from  SparkFun_MS5803_I2C import ms5803py # taken from https://pypi.org/project/ms5803py/#
+
+    #/// Version string: be sure to update this version number EVERY time you make any code change,
+    #/// period, which isn't just a comment change! Document it in the Software Changelog in the main
+    #/// AmboVent readme.
+    ##define VERSION_STR "v0.1.0"#
+VERSION_STR =  "v0.1.0"
+#   // TODO(ElectricRCAircraftGuy): get rid of nearly every single one of these #defines! They should
+#   // be `constexpr` variables instead, with the sole exception of ones which are used for variable
+#   // compilation, such as those which exclude certain portions of code based on hardware
+#   // configuration. (Even this may not be true though, as using bool may still allow the compiler
+#   // to optimize out those sections. Do some compilation testing when you get to it).#
+
+#   // System Configuration#
+
+#   /// Set to true for "full system" (the default), or to false for "partial system"--potentiometer
+#   /// installed on pulley, no potentiometers... <---? TODO(@ElectricRCAircraftGuy): I request further
+#   /// explanation from @nimrod46--please update this description to be more clear.
+FULL_CONFIGURATION = False
+#/// Set to true if you have installed an I2C pressure sensor
+PRESSURE_SENSOR_AVAILABLE = False
+# /// Set to true to send unique ID for 10 seconds at startup, false otherwise
+CENTRAL_MONITOR_SYSTEM = False
